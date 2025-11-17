4a3242b06f6847062942f9f6f853fbc0e25a40d5
Adrien <adrien.blandeau@viacesi.fr>
2025-11-17 14:10:51 +0100

    Création CLaddition

---
+++ composants/CLaddition.cpp
@@ -0,0 +1,9 @@
+#include "CLaddition.h"
+
+
+namespace composants{
+    CLaddition::CLaddition(){};
+    double CLaddition::addition(double* p1, int p2){
+        return *p1+p2;
+    }
+}
---
+++ composants/CLaddition.h
@@ -0,0 +1,11 @@
+#pragma once 
+
+
+namespace composants{
+
+    class CLaddition{
+        public:
+            CLaddition();
+            double addition(double*, int);
+    };
+}
