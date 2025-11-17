fc80345d6f4c390f81ce4fe8565467f5788ef648
Alexandre Marcastel <amarcastel@cesi.fr>
2025-11-17 15:07:12 +0100

    Corrections des erreurs

--- composants/CLaddition.cpp
+++ composants/CLaddition.cpp
@@ -1,10 +1,11 @@
 #include "CLaddition.h"
 
+namespace app{
+    namespace composants{
+        CLaddition::CLaddition(){};
+        double CLaddition::addition(double* p1, int p2){
+            return *p1+p2; /* on comment pour le fun*/
+        }
 
-namespace composants{
-    CLaddition::CLaddition(){};
-    double CLaddition::addition(double* p1, int p2){
-        return *p1+p2; /* on comment pour le fun*/
     }
-
 }
--- composants/tva.h
+++ composants/tva.h
@@ -1,14 +1,18 @@
-namespace tva {
-    class TVA {
-        private :
-            const double tva = 1.2;
-            double prix;
-        public :
-        TVA();
-        TVA(double, double);
-        ~TVA();
-        double calculTVA(double){
-            return prix*tva;
-        }
-    };
+#pragma once
+
+namespace app {
+    namespace composants{
+        class TVA {
+            private :
+                const double tva = 1.2;
+                double prix;
+            public :
+            TVA();
+            TVA(double, double);
+            ~TVA();
+            double calculTVA(double){
+                return prix*tva;
+            }
+        };
+    }
 }
