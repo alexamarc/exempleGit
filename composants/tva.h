4d0f1b63bbe6f306a3e0c1390713c81422b9d034
Pennington Noah <pennington.noah16@gmail.com>
2025-11-17 13:59:26 +0100

    ajout de la classe tva

---
+++ composants/tva.h
@@ -0,0 +1,14 @@
+namespace tva {
+    class TVA {
+        private :
+            const double tva = 1.2;
+            double prix;
+        public :
+        TVA();
+        TVA(double, double);
+        ~TVA();
+        double calculTVA(double){
+            return prix*tva;
+        }
+    };
+}
